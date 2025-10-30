6658d6567673ac4ba5380bc894d04aa892420633
Shamail Abbas <shamailabbas61@gmail.com>
2025-10-30 23:34:26 +0500

    added sc

--- nodejs-app/telemetry.js
+++ nodejs-app/telemetry.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { NodeSDK } = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-grpc");
